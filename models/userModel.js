8b6e309d1926eaa36f92f6b0f37ea1bbda26b1fe
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-06 23:22:46 +0530

    userschema

--- models/userModel.js
+++ models/userModel.js
@@ -3,8 +3,15 @@ const mongoose=require('mongoose')
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
-        require:[true,"name is required"],
+        required:[true,"name is required"],
         trim:true,
         maxlength:[30,"name musht be less than 30 chars"]
+    },
+    email:{
+        type:String,
+        required:[true, "email is required"],
+        unique:true
     }
-})
+});
+
+module.exports = mongoose.model('User',userSchema);
