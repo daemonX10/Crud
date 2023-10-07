063b6c6fddfd5ccfdf60ed054908e603afbb6776
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-07 20:10:59 +0530

    bug fix of multiple response to single res

--- Controller/userController.js
+++ Controller/userController.js
@@ -4,37 +4,39 @@ exports.home=(req,res)=>{
     res.send("hello world");
 }
 
-exports.createUser = async (req,res)=>{
+exports.createUser = async (req, res) => {
     try {
-        const {name,email} = req.body;
-        if (!name || !email){
-            throw new Error ("name and email is required ");
+        const {name, email} = req.body
+
+        if (!name || !email) {
+            throw new Error('Name and email are required')
         }
 
-        //  Check if user is already registered
-
-        const userExits = await User.findOne({email});
-
-        if(userExits){
-            throw new Error ("user already exits");
+        //check if user is already registered
+        const userExists = await User.findOne({email})
+        // console.log(userExists);
+        if (userExists) {
+            throw new Error('User already exists')
         }
 
         const user = await User.create({
             name,
             email
         })
+        console.log(user);
 
-        res.send(201).json({
+        res.status(201).json({
             success: true,
-            message:"User created success ",
+            message: "User created successfully",
             user
-
         })
-        
+
+
     } catch (error) {
         console.log(error);
         res.status(400).json({
-            message: error.message
+            success: false,
+            messsage: error.message
         })
     }
 }
