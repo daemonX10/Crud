908cbccf6c15d9871540552ad74f94dcbf6a28ad
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-07 16:49:29 +0530

    create user  controller

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,5 +1,40 @@
-
+const User = require('../models/userModel.js');
 
 exports.home=(req,res)=>{
     res.send("hello world");
+}
+
+exports.createUser = async (req,res)=>{
+    try {
+        const {name,email} = req.body;
+        if (!name || !email){
+            throw new Error ("name and email is required ");
+        }
+
+        //  Check if user is already registered
+
+        const userExits = User.findOne({email});
+
+        if(userExits){
+            throw new Error ("user already exits");
+        }
+
+        const user = User.create({
+            name,
+            email
+        })
+
+        res.send(201).json({
+            success: true,
+            message:"User created success ",
+            user
+
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            message: error.message
+        })
+    }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express= require('express');
-const { home } = require('../Controller/userController.js');
+const { home, createUser } = require('../Controller/userController.js');
 
 const router = express.Router();
 
 router.get('/', home);
+router.post("/createUser", createUser);
 
 module.exports = router;
